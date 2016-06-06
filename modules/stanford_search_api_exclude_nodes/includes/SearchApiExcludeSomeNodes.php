25a73fd1cd0526c4253be306ffeac66bf036f8f3
Greg Garvey <ggarvey@stanford.edu>
2016-06-06 12:16:09 -0700

    added stanford_search_api_exclude_nodes as an optional module to be able to exclude selected nodes from a solr index

---
+++ modules/stanford_search_api_exclude_nodes/includes/SearchApiExcludeSomeNodes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * SearchApiExcludeSomeNodes provides mechanism for not indexing nodes.
+ */
+class SearchApiExcludeSomeNodes extends SearchApiAbstractAlterCallback {
+
+  public function supportsIndex(SearchApiIndex $index) {
+    return $index->getEntityType() === 'node';
+  }
+
+  public function alterItems(array &$items) {
+    foreach ($items as $k => $item) {
+      $wrapper = entity_metadata_wrapper('node', $item);
+      if(isset($wrapper->field_s_apachesolr_index) && $wrapper->field_s_apachesolr_index->value() == '1') {
+        unset($items[$k]);
+      }
+    }
+  }
+
+}
